985bc578613acb781827ce4144f398a6dc734805
Justramin <143264759+Justramin@users.noreply.github.com>
2024-11-27 11:59:45 +0530

    Creat 4 new pages

--- controllers/userControllers/userConsulting.js
+++ controllers/userControllers/userConsulting.js
@@ -2,16 +2,47 @@
 
 
 
-const userConsulting = async (req, res) => {
+const Business_Fix = async (req, res) => {
     try {
 
-        res.render('consulting');
+        res.render('consulting1');
     } catch (error) {
-        console.error('Error in userConsulting:', error);
+        console.error('Error in Business_Fix :', error);
         res.redirect('/userError');
     }
 };
 
+const Career_Cadence = async (req, res) => {
+    try {
+        res.render('consulting2');
+    } catch (error) {
+        console.error('Error in Career_Cadence:', error);
+        res.redirect('/userError');
+    }
+};
+
+const PrepNLift = async (req, res) => {
+    try {
+
+        res.render('consulting3');
+    } catch (error) {
+        console.error('Error in PrepNLift:', error);
+        res.redirect('/userError');
+    }
+};
+
+const Institute2Industry = async (req, res) => {
+    try {
+
+        res.render('consulting4');
+    } catch (error) {
+        console.error('Error in Institute2Industry:', error);
+        res.redirect('/userError');
+    }
+};
 module.exports = {
-    userConsulting
+    Business_Fix,
+    Career_Cadence,
+    PrepNLift,
+    Institute2Industry
 };
--- route/userRoute/userRoute.js
+++ route/userRoute/userRoute.js
@@ -13,7 +13,7 @@ const userTestimonial = require('../../controllers/userControllers/userTestimoni
 
 router.get('/',userHome.userHome)
 router.get('/home',userHome.userHome)
-router.get('/consulting',userConsulting.userConsulting)
+
 router.get('/events',userEvents.userEvents)
 router.get('/details',userDetails.userDetails)
 router.get('/contact',usercontact.usercontact)
@@ -23,5 +23,9 @@ router.get('/getHired',userTestimonial.userGetHired)
 router.post('/getHiredPost',userTestimonial.getHiredPost)
 router.post('/contactPost',usercontact.contactPost)
 
+router.get('/Business_Fix',userConsulting.Business_Fix)
+router.get('/Career_Cadence',userConsulting.Career_Cadence)
+router.get('/PrepNLift',userConsulting.PrepNLift)
+router.get('/Institute2Industry',userConsulting.Institute2Industry)
 
 module.exports = router
